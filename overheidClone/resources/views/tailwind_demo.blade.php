953d4350037633a450bd8bc6965b00c6af0a30f0
lollerdon <sami2007tabib@gmail.com>
2025-09-16 14:09:09 +0200

    .

---
+++ overheidClone/resources/views/tailwind_demo.blade.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Clone</title>
+    @vite('resources/css/app.css')
+</head>
+<body class="bg-black">
+<div class="bg-white text-blue-900 flex items-center justify-center flex-row gap-2">
+    <img class="content-center"
+         src="https://www.rijksoverheid.nl/binaries/content/gallery/rijksoverheid/channel-afbeeldingen/logos/beeldmerk-rijksoverheid-desktop.svg"
+         alt="logo"/>
+    <p class="font-semibold">Rijksoverheid</p>
+</div>
+<div class="flex items-center justify-center flex-row gap-2">
+    <nav class="flex items-center justify-center flex-row gap-4 p-4 bg-blue-900 text-white w-full">
+        <a href="#" class="hover:underline">Home</a>
+        <a href="#" class="hover:underline">Actueel</a>
+        <a href="#" class="hover:underline">Onderwerpen</a>
+        <a href="#" class="hover:underline">Ministeries</a>
+        <a href="#" class="hover:underline">Regeringen</a>
+        <a href="#" class="hover:underline">Documenten</a>
+    </nav>
+</div>
+<img class="w-full"
+     src="https://www.rijksoverheid.nl/binaries/large/content/gallery/rijksoverheid/content-afbeeldingen/home/evergreens/schijnzelfstandigheid-3.jpg"
+     alt="header image"/>
+</body>
+</html>
