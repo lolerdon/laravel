2097e8d3aae9fb8bc6c4678a604f41aa3b2bc498
lollerdon <sami2007tabib@gmail.com>
2025-09-26 11:34:26 +0200

    .

---
+++ basic_controller/resources/views/home.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <title>Home</title>
+</head>
+<body>
+<header class="header bg-blue-500 text-white p-4">
+    <div class="container mx-auto flex justify-between items-center">
+        <a href="{{ route("home") }}">Home</a>
+        <a href="{{ route("about") }}">About us</a>
+        <a href="{{ route("contact") }}">Contact</a>
+        <a href="{{ route("our_team") }}">Our Team</a>
+    </div>
+</header>
+</body>
+</html>
--- basic_controller/routes/web.php
+++ basic_controller/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get("/", [PageController::class, "index"])->name("home");
+Route::get("/about", [PageController::class, "about"])->name("about");
+Route::get("/contact", [PageController::class, "contact"])->name("contact");
+Route::get("/our_team", [PageController::class, "our_team"])->name("our_team");
